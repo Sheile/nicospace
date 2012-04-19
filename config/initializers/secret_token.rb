1bd50aed569e79c9ca8ad09f3abb23f989d0a39c
Sheile <sheile1024@gmail.com>
2012-04-19 09:53:11 +0900

    generate rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nicospace::Application.config.secret_token = '4a43017144d3855dfeacb8aa64773ac0eca44b0e8bbbae28800fdb244b807aafb9d1d893da4ccd7eb95ea9f1e4244022b9a23dab18cfb5818bae8c31eea3b77a'
