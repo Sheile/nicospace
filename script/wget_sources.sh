b2d3ab177eeee256bc21cda2545465749c39fc6b
Sheile <sheile1024@gmail.com>
2012-06-03 10:16:20 +0900

    HTML取得時のエラーチェックを追加

--- script/wget_sources.sh
+++ script/wget_sources.sh
@@ -3,6 +3,12 @@
 cd $(dirname $0)
 cd ..
 
-wget http://spaceflightnow.com/tracking/index.html -q -O tmp/index.html
-wget http://www.nasa.gov/directorates/heo/reports/iss_reports/ -q -O tmp/issstatusreport.html
+wget http://spaceflightnow.com/tracking/index.html -q -O tmp/temp.html
+if [ $? -eq 0 ]; then
+  mv tmp/temp.html tmp/index.html
+fi
 
+wget http://www.nasa.gov/directorates/heo/reports/iss_reports/ -q -O tmp/temp.html
+if [ $? -eq 0 ]; then
+  mv tmp/temp.html tmp/issstatusreport.html
+fi
